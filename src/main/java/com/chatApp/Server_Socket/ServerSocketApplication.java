9651b8e9dd7b16ac8f3dc638b68eb09e40ec9d2b
Fadeusi <108889982+Fadeusi@users.noreply.github.com>
2024-08-23 18:39:09 -0400

    Update ServerSocketApplication.java

--- src/main/java/com/chatApp/Server_Socket/ServerSocketApplication.java
+++ src/main/java/com/chatApp/Server_Socket/ServerSocketApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//test
 public class ServerSocketApplication {
 
 	public static void main(String[] args) {
